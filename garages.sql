e5af5b922c03bc1e3e3b9058c21ef483e4959a4d
Banditul <neagoeciprian16@yahoo.com>
2017-10-31 18:57:19 +0200

    no message

--- garages.sql
+++ garages.sql
@@ -35,7 +35,6 @@ CREATE TABLE `garages` (
   `eZ` float NOT NULL DEFAULT '0',
   `Price` int(11) NOT NULL DEFAULT '0',
   `Size` tinyint(4) NOT NULL
-  `VirtualWorld` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
